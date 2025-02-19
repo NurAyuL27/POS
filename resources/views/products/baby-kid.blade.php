56d1492c1104773007d9c1f14eb1a0e4eeef6914
Nuraayu <nurayu2718@gmail.com>
2025-02-19 16:26:22 +0700

    Update

---
+++ resources/views/products/baby-kid.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Baby Kid</title>
+</head>
+<body>
+    <h1>Daftar Produk: Baby Kidd</h1>
+</body>
+</html>
---
+++ resources/views/products/food-beverage.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Food & Beverage</title>
+</head>
+<body>
+    <h1>Daftar Produk: Food & Beveragee</h1>
+</body>
+</html>
